e5fc3fa316f1269bc00d7dae5d5ade4f3a8e5fd4
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-02-02 21:03:20 +0900

    made response to different screen sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
@@ -13,9 +19,29 @@ Ingredient.create(name: "tequila")
 Ingredient.create(name: "vodka")
 Ingredient.create(name: "soju")
 
-Cocktail.create(name: "mojito")
-Cocktail.create(name: "bloody mary")
-Cocktail.create(name: "margarita")
+file = URI.open('https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1886&q=80')
+
+one = Cocktail.create(name: "mojito")
+
+one.photo.attach(io: file, filename: 'cocktail', content_type: 'image/jpeg')
+
+one.save
+
+file_two = URI.open('https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1886&q=80')
+
+two = Cocktail.create(name: "bloody mary")
+
+two.photo.attach(io: file_two, filename: 'cocktail', content_type: 'image/jpeg')
+
+two.save
+
+file_three = URI.open('https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1886&q=80')
+
+three = Cocktail.create(name: "margarita")
+
+three.photo.attach(io: file_three, filename: 'cocktail', content_type: 'image/jpeg')
+
+three.save
 
 Dose.create(description: "6 leaves", cocktail_id: 1, ingredient_id: 3)
 
